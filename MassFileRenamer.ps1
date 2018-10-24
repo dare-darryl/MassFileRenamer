101c485ac1e62d8296a160fc2c13993db3b7f1f5
Darryl Sw <darryl_sw@hotmail.com>
2018-10-24 12:02:08 -0400

    Add MassFileRenamer
    Initial code for massive file renamer.
    
    Reads input from user, and filters all files from the current directory given the filter.

---
+++ MassFileRenamer.ps1
@@ -0,0 +1,21 @@
+﻿# Written by Darryl
+
+function get-all-files ($filter){
+    # Selecting all files in directory
+    $files = Get-Item *
+
+    # Removing all directories from list
+    $files = $files | Where-Object {($_.attributes -notlike "Directory") -and ($_.Name -like $filter)}
+    
+    return $files
+}
+
+function get-input {
+    $input = Read-Host -Prompt "Filenames to search"
+    return $input
+}
+
+# Initial Filtering
+$input = get-input
+
+get-all-files("*" + $input +"*")
