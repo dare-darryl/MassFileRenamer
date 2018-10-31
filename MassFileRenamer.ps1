67d5412deb3bea09d147b3e74546a67d76e0f886
Darryl Sw <darryl_sw@hotmail.com>
2018-10-31 01:13:42 -0400

    Update MassFileRenamer.ps1
    Simple working copy

--- MassFileRenamer.ps1
+++ MassFileRenamer.ps1
@@ -1,6 +1,7 @@
 ﻿# Written by Darryl
 
-function get-all-files ($filter){
+function get-all-files {
+    param($filter)
     # Selecting all files in directory
     $files = Get-Item *
     
@@ -11,28 +12,100 @@ function get-all-files ($filter){
 }
 
 function get-input {
-    $input = Read-Host -Prompt "Filenames to search" 
+    param($prompt)
+    $fileSearch = Read-Host -Prompt $prompt
 
-    if ($input -eq "") {
-        Write-Host "No search detected" -ForegroundColor Red
-        $input = get-input
+
+    if ($fileSearch -eq "") {
+        Write-Host "No input detected" -ForegroundColor Red
+        $fileSearch = get-input $prompt
     }
 
-    return $input
+    return $fileSearch
 }
 
-function print-filenames($files, $input){
+function print-filenames{
+    param($files, $fileSearch, $replaceItem)
+
     for($i = 0; $i -lt $files.Length; $i++)
     {
-        Write-Host $files[$i].Name -ForegroundColor Yellow
+        $temp = $files[$i].Name
+        $filenamefrag = $temp -csplit $fileSearch
+        
+        Write-Host -NoNewline $filenamefrag[0] -ForegroundColor Yellow
+        Write-Host -NoNewline $replaceItem -ForegroundColor Red
+
+        for($j = 1; $j -lt $filenamefrag.Length; $j++)
+        {
+            if($j -ne 1)
+            {
+                Write-Host -NoNewline $fileSearch -ForegroundColor Yellow
+            }
+    
+            Write-Host -NoNewline $filenamefrag[$j] -ForegroundColor Yellow
+        }
+
+        Write-Host
     }
 }
 
-# Initial Filtering
-$input = get-input
-Write-Host
+function rename-filenames{
+    param($files, $fileSearch, $replaceItem)
 
-$files = @(get-all-files("*" + $input +"*"))
+    for($i = 0; $i -lt $files.Length; $i++)
+    {
+        $temp = $files[$i].Name
+        $filenamefrag = $temp -csplit $fileSearch
+
+        $rebuiltFilename = $filenamefrag[0]
+        $rebuiltFilename += $replaceItem
+
+        for($j = 1; $j -lt $filenamefrag.Length; $j++)
+        {
+            if($j -ne 1)
+            {
+                $rebuiltFilename += $fileSearch
+            }
+    
+            $rebuiltFilename += $filenamefrag[$j]
+        }
+
+        $files[$i] | Rename-Item -NewName $rebuiltFilename
+        Write-Host "Renamed: $rebuiltFilename"
+    }
+}
+
+
+
+# Initial Filtering
+$fileSearch = get-input "Filenames to search"
+Write-Host Input is $fileSearch
+
+$files = @(get-all-files("*" + $fileSearch +"*"))
 
 Write-Host "Files Found:" -ForegroundColor Green
-print-filenames($files)
+print-filenames $files $fileSearch $fileSearch
+
+# Read Proposed Changes
+Write-Host
+$replace = Read-Host "String to replace with"
+Write-Host "Files to Rename:" -ForegroundColor Green
+print-filenames $files $fileSearch $replace
+Write-Host
+
+# Confirm Changes
+while($true){
+    $confirmrn = get-input "Confirm change (y/n)"
+    if ($confirmrn -like "y*" -or $confirmrn -like "n*"){
+        break
+    }
+
+    Write-Host "Invalid response"
+}
+
+if ($confirmrn -like 'n*') {
+    exit
+}
+
+# Write Changes
+rename-filenames $files $fileSearch $replace
